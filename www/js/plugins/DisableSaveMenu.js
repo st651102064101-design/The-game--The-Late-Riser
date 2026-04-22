8e2269a459171591b8b1a9392da69504da2b041b
PHATPHIMONs <phatphimon2244@gmail.com>
2026-04-22 22:43:49 +0700

    edit

--- www/js/plugins/DisableSaveMenu.js
+++
@@ -1,9 +0,0 @@
-
-
-(function() {
-    var _Window_MenuCommand_addMainCommands = Window_MenuCommand.prototype.addMainCommands;
-    Window_MenuCommand.prototype.addMainCommands = function() {
-        _Window_MenuCommand_addMainCommands.call(this);
-        this.removeCommand(TextManager.save);
-    };
-})();
